284e35d7d313878928818c880792dab72f71f414
Mohammad Azam <azamsharp@gmail.com>
2023-05-02 14:49:49 -0500

    Added GroceryItemRequestDTO

--- Sources/GroceryAppShared/DTOs.swift
+++ Sources/GroceryAppShared/DTOs.swift
@@ -30,3 +30,16 @@ public struct GroceryCategoryResponseDTO: Codable {
         self.noOfItems = noOfItems
     }
 }
+
+public struct GroceryItemRequestDTO: Codable {
+    
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
