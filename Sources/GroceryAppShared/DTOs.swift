f3d3b0754cae25b8c8c5cf0751558b6b1933c078
Mohammad Azam <azamsharp@gmail.com>
2023-05-02 13:48:45 -0500

    added public to properties

--- Sources/GroceryAppShared/DTOs.swift
+++ Sources/GroceryAppShared/DTOs.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct GroceryCategoryRequestDTO: Codable {
-    let title: String
-    let color: String
+    public let title: String
+    public let color: String
     
     public init(title: String, color: String) {
         self.title = title
